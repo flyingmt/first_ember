bf0e1a94d55fb9fcac632a9c02872c0e6b848a9d
Jun-Hee Lee <flyingmt@gmail.com>
2014-09-30 10:42:41 -0700

    Change title to a template variable.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,7 @@
 var FirstEmber = window.FirstEmber = Ember.Application.create();
 
+RocknrollcallYeoman.applicationName = "Rock'n'Rool Call";
+
 /* Order and include as you please. */
 require('scripts/controllers/*');
 require('scripts/store');
