69246c66a44aebd757ba01288385782c39b7827c
Jun-Hee Lee <flyingmt@gmail.com>
2014-09-30 13:45:10 -0700

    Tracking the book.

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -1,6 +1,12 @@
 
 FirstEmber.Router.map(function () {
-    this.route('search-results');
-    this.route('artist');
-    this.route('song');
+    this.route('search-results', {
+        path: 'search/:term'
+    });
+    this.route('artist', {
+        path: 'artist/:endid'
+    });
+    this.route('song', {
+        path: 'song/:endid'
+    });
 });
