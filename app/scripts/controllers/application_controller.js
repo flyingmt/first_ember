ef792a8e7fe92e464f57a1dcba9a57a8e36042a8
Jun-Hee Lee <flyingmt@gmail.com>
2014-09-30 14:52:47 -0700

    Tracking the book.

--- app/scripts/controllers/application_controller.js
+++ app/scripts/controllers/application_controller.js
@@ -4,8 +4,17 @@
 
 FirstEmber.ApplicationController = Em.ObjectController.extend({
     searchTerms: '',
-    applicationName: "Rock'n'Roll Call",
-    actions: {
+    applicationName: function() {
+        var st = this.get('searchTerms');
+        if (st) {
+            return st + "???";
+        }
+        else {
+            return "Rock'n'Roll Call";
+        }
+    }.property('searchTerms'),
+    actions :
+    {
         submit: function() {
             this.transitionToRoute('search-results',this.get('searchTerms'));
         }
