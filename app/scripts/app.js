e012b06ea83a920ca564bb1872be8c5bba6c2dcf
Jun-Hee Lee <flyingmt@gmail.com>
2014-09-30 14:04:19 -0700

    Tracking the book.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,33 +2,6 @@ var FirstEmber = window.FirstEmber = Ember.Application.create();
 
 FirstEmber.applicationName = "Rock'n'Rool Call";
 
-/*
-FirstEmber.dummySearchResultsArtists = [
-    {
-        id: 1,
-        name: 'Tom Waits',
-        type: 'artist',
-        enid: 'ARERLPG1187FB3BB39',
-        hotttnesss: '1'
-    },
-    {
-        id: 2,
-        name: 'Thomas Alan Waits',
-        type: 'artist',
-        enid: 'ARERLPG1187FB3BB39',
-        hotttnesss: '.89'
-    },
-    {
-        id: 3,
-        name: 'Tom Waits w/ Keith Richards',
-        type: 'artist',
-        enid: 'ARMPVNN13CA39CF8FC',
-        hotttnesss: '.79'
-    }
-];
-*/
-
-
 /* Order and include as you please. */
 require('scripts/controllers/*');
 require('scripts/store');
---
+++ app/scripts/models/artist.js
@@ -0,0 +1,13 @@
+/**
+ * Created by flyingmt on 2014. 9. 30..
+ */
+
+FirstEmber.Artist = Em.Object.extend({
+    id: null,
+    name: null,
+    enid: null,
+    biography: null,
+    hotttnesss: null,
+    image: null,
+    videos: null
+});
